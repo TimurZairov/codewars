47f6105582e31fab7002df051183c17c304addd7
Timur <zairovne@gmail.com>
2022-12-24 16:37:03 +0500

    1

---
+++ index.js
@@ -0,0 +1,11 @@
+// function countBy(x, n) {
+//     let z = [];
+//     for(let i=1; i<=n; i++){
+//         let num = x*i
+//         z.push(num)
+//     }
+//     console.log(z)
+//     return z;
+// }
+//
+// countBy(1,10)
