3b43e412e5549063b1c2a9f08b45f1b7f1c8d5fe
Timur <zairovne@gmail.com>
2022-12-27 15:03:56 +0500

    18

--- index.js
+++ index.js
@@ -196,3 +196,10 @@
 // }
 //
 // makeNegative(42)
+
+// function twiceAsOld(dadYearsOld, sonYearsOld) {
+//     // your code here
+//     return Math.abs(dadYearsOld-sonYearsOld * 2)
+//
+// }
+// twiceAsOld(55, 30)
