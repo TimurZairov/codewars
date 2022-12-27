59dc5e46dd8eafbf694ec32cec6368851d936595
Timur <zairovne@gmail.com>
2022-12-27 15:23:39 +0500

    19

--- index.js
+++ index.js
@@ -203,3 +203,16 @@
 //
 // }
 // twiceAsOld(55, 30)
+
+// function positiveSum(arr) {
+//     let sum = 0
+//     for (let i =0; i < arr.length; i++){
+//         if(arr[i] < 0) {
+//             continue
+//         }
+//         sum = sum + arr[i]
+//     }
+//     return sum
+// }
+//
+// positiveSum([1,-2,3,4,5])
